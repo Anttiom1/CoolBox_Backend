c70f60278d3e045c25b79d9ec852f76f1766e38b
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-03-18 22:10:42 +0200

    Update coolbox_db.sql

--- coolbox_db.sql
+++ coolbox_db.sql
@@ -18,14 +18,51 @@ USE `CoolBox` ;
 -- Table `CoolBox`.`sensor_dim`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `CoolBox`.`sensor_dim` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `sensor_id` VARCHAR(45) NOT NULL,
   `device_id` VARCHAR(45) NOT NULL,
   `device_name` VARCHAR(45) NOT NULL,
-  `sensor_id` VARCHAR(45) NOT NULL,
-  `sensor_name` VARCHAR(45) NOT NULL,
   `unit_name` VARCHAR(45) NOT NULL,
   `unit_value` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`sensor_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `CoolBox`.`timestamp_dim`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CoolBox`.`timestamp_dim` (
+  `timestamp_key` INT NOT NULL AUTO_INCREMENT,
+  `year` INT NOT NULL,
+  `month` INT NOT NULL,
+  `week` INT NOT NULL,
+  `day` INT NOT NULL,
+  `hour` INT NOT NULL,
+  `minute` INT NOT NULL,
+  `sec` INT NOT NULL,
+  `microsec` INT NOT NULL,
+  PRIMARY KEY (`timestamp_key`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `CoolBox`.`measurement_fact`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CoolBox`.`measurement_fact` (
+  `timestamp_key` INT NOT NULL,
+  `sensor_id` VARCHAR(45) NOT NULL,
+  `value` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`timestamp_key`, `sensor_id`),
+  INDEX `fk_measurement_fact_sensor_dim1_idx` (`sensor_id` ASC),
+  CONSTRAINT `fk_measurement_fact_timestamp_dim`
+    FOREIGN KEY (`timestamp_key`)
+    REFERENCES `CoolBox`.`timestamp_dim` (`timestamp_key`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_measurement_fact_sensor_dim1`
+    FOREIGN KEY (`sensor_id`)
+    REFERENCES `CoolBox`.`sensor_dim` (`sensor_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
