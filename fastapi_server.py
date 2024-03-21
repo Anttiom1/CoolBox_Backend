81ed9f055d460d166ba24f458a09059d4e4d8eb8
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-03-21 14:14:11 +0200

    added sisälämpötila endpoints

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from sqlalchemy import text
 from db import DB
 
@@ -17,5 +17,37 @@ async def get_all_measurements(db: DB):
     rows = db.execute(text(_query_str))
     data = rows.mappings().all()
     return {"data" : data}
-    
 
+@app.get("/api/temperature/indoors/latest")
+async def get_latest_temperature_indoors(db: DB):
+    _query_str = "\
+    SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec \
+    FROM `measurement_fact`\
+    JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+    JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+    WHERE device_name = 'sisäilma' \
+    AND unit_value = '°C' \
+    AND measurement_fact.timestamp_key = \
+        (   SELECT MAX(timestamp_dim.timestamp_key) FROM measurement_fact \
+            JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+            JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id  \
+            WHERE sensor_dim.device_name = 'sisäilma'\
+            AND sensor_dim.unit_name = 'lämpötila'\
+        )"
+    rows = db.execute(text(_query_str))
+    data = rows.mappings().all()
+    return {"data" : data}
+    
+@app.get("/api/temperature/indoors/{week}")
+async def get_weekly_temperature_indoors(week: int, db: DB):
+    _query_str = f"\
+        SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec FROM measurement_fact \
+        JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+        JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+        WHERE sensor_dim.device_name = 'sisäilma' \
+        AND sensor_dim.unit_name = 'lämpötila' \
+        AND timestamp_dim.year = 2024 \
+        AND timestamp_dim.week = :week" 
+    rows = db.execute(text(_query_str), {"week": week})  
+    data = rows.mappings().all()
+    return {"data": data}
