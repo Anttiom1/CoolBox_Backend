493c92669df225a7492c456402e38d1ac457172a
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-03-25 13:29:40 +0200

    more temperature endpoints

--- fastapi_server.py
+++ fastapi_server.py
@@ -45,7 +45,41 @@ async def get_weekly_temperature_indoors(week: int, db: DB):
         JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
         JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
         WHERE sensor_dim.device_name = 'sisäilma' \
-        AND sensor_dim.unit_name = 'lämpötila' \
+        AND sensor_dim.unit_name = 'lämpötila'\
+        AND timestamp_dim.year = 2024 \
+        AND timestamp_dim.week = :week" 
+    rows = db.execute(text(_query_str), {"week": week})  
+    data = rows.mappings().all()
+    return {"data": data}
+
+@app.get("/api/temperature/outdoors/latest")
+async def get_latest_temperature_outdoors(db: DB):
+    _query_str = "\
+        SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec \
+        FROM `measurement_fact`\
+        JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+        JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+        WHERE device_name = 'Sääasema masto' \
+        AND unit_value = '°C' \
+        AND measurement_fact.timestamp_key = \
+        (   SELECT MAX(timestamp_dim.timestamp_key) FROM measurement_fact \
+            JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+            JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id  \
+            WHERE sensor_dim.device_name = 'Sääasema masto'\
+            AND sensor_dim.unit_name = 'Lämpötila'\
+        )" 
+    rows = db.execute(text(_query_str))
+    data = rows.mappings().all()
+    return {"data" : data}
+
+@app.get("/api/temperature/outdoors/{week}")
+async def get_weekly_temperature_outdoors(week: int, db: DB):
+    _query_str = f"\
+        SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec FROM measurement_fact \
+        JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+        JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+        WHERE sensor_dim.device_name = 'Sääasema masto' \
+        AND sensor_dim.unit_name = 'Lämpötila'\
         AND timestamp_dim.year = 2024 \
         AND timestamp_dim.week = :week" 
     rows = db.execute(text(_query_str), {"week": week})  
