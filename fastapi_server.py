4905195488897fdaeea7c8b6b0d3a44c5fbe804f
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-03-26 17:44:51 +0200

    Update fastapi_server.py

--- fastapi_server.py
+++ fastapi_server.py
@@ -38,6 +38,22 @@ async def get_weekly_temperature_indoors(week: int, db: DB):
     data = rows.mappings().all()
     return {"data": data}
 
+@app.get("/api/temperature/indoors/{month}/{day}")
+async def get_daily_temperature_indoors(month: int, day: int, db: DB):
+    _query_str = f"\
+        SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec FROM measurement_fact \
+        JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+        JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+        WHERE sensor_dim.device_name = 'sisäilma' \
+        AND sensor_dim.unit_name = 'lämpötila'\
+        AND timestamp_dim.year = 2024 \
+        AND timestamp_dim.month = :month\
+        AND timestamp_dim.day = :day" 
+    rows = db.execute(text(_query_str), {"month": month, "day": day})  
+    data = rows.mappings().all()
+    return {"data": data}
+
+
 @app.get("/api/temperature/outdoors/latest")
 async def get_latest_temperature_outdoors(db: DB):
     _query_str = "\
@@ -71,6 +87,22 @@ async def get_weekly_temperature_outdoors(week: int, db: DB):
     rows = db.execute(text(_query_str), {"week": week})  
     data = rows.mappings().all()
     return {"data": data}
+
+@app.get("/api/temperature/outdoors/{month}/{day}")
+async def get_daily_temperature_outdoors(month: int, day: int, db: DB):
+    _query_str = f"\
+        SELECT device_name, unit_name, value, unit_value, year, month, day, hour, minute, sec FROM measurement_fact \
+        JOIN timestamp_dim ON measurement_fact.timestamp_key = timestamp_dim.timestamp_key \
+        JOIN sensor_dim ON measurement_fact.sensor_id = sensor_dim.sensor_id \
+        WHERE sensor_dim.device_name = 'Sääasema masto' \
+        AND sensor_dim.unit_name = 'Lämpötila'\
+        AND timestamp_dim.year = 2024 \
+        AND timestamp_dim.month = :month\
+        AND timestamp_dim.day = :day" 
+    rows = db.execute(text(_query_str), {"month": month, "day": day})  
+    data = rows.mappings().all()
+    return {"data": data}
+
 
 @app.get("/api/energy/{month}/{day}")
 async def get_energy_consumption_by_day(month: int, day: int, db: DB):
